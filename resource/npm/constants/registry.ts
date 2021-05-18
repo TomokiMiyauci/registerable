d0c6dd5f3cd809f9aa8d7876e6d44aebc27682df
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2021-05-18 21:04:37 +0900

    :recycle:  Move to resource dir

---
+++ resource/npm/constants/registry.ts
@@ -0,0 +1,4 @@
+// Copyright 2021-present the Nameable authors. All rights reserved. MIT license.
+
+export const NPM = "npm";
+export const NPM_BASE_URL = "https://registry.npmjs.org/";
--- resource/npm/query.ts
+++ resource/npm/query.ts
@@ -1,6 +1,6 @@
 // Copyright 2021-present the Nameable authors. All rights reserved. MIT license.
-import { NPM_BASE_URL } from "../constants.ts";
-import { has, ifElse, props } from "../deps.ts";
+import { NPM_BASE_URL } from "./constants/registry.ts";
+import { has, ifElse, props } from "../../deps.ts";
 type Response = {
   name: string;
   "dist-tags": Record<string, string>;
@@ -10,12 +10,12 @@ type Response = {
   readmeFilename: string;
   time: Record<string, string>;
   versions: Record<string, string>;
-  _attachments: {};
+  _attachments: Record<string, string>;
   _rev: string;
   _id: string;
 } | { "error": "Not found" };
 
-const query = async (search: string) => {
+const query = async (search: string): Promise<boolean | Error> => {
   const modulesUrl = new URL(search, NPM_BASE_URL);
 
   try {
@@ -27,9 +27,9 @@ const query = async (search: string) => {
       true,
     );
 
-    return ["npm", isAvailable] as const;
-  } catch {
-    return ["npm", false] as const;
+    return isAvailable;
+  } catch (e) {
+    return e as Error;
   }
 };
 
