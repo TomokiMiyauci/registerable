c6b14ec429d2b49641874574635bfebb97a2789a
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2021-05-19 14:38:03 +0900

    :sparkles:  Add validation for deno land module name

--- resource/deno_land/query.ts
+++ resource/deno_land/query.ts
@@ -1,6 +1,6 @@
 // Copyright 2021-present the Nameable authors. All rights reserved. MIT license.
 import { DENO_LAND_BASE_URL } from "./constants/registry.ts";
-import { and, isBoolean, N, NN, or, props } from "../../deps.ts";
+import { N, or, props, tryCatch } from "../../deps.ts";
 
 type Response = {
   success: boolean;
@@ -11,28 +11,18 @@ type Response = {
   };
 };
 
-const simpleCompare = async (search: string) => {
+const simpleCompare = (search: string) => {
   const modulesUrl = new URL(search, DENO_LAND_BASE_URL);
 
-  try {
+  return tryCatch<Promise<boolean>, Error>(async () => {
     const res = await fetch(modulesUrl.toString());
     const { success, data } = await res.json() as Response;
     const isAvailable = or(N(success), () => props("name", data) !== search);
     return isAvailable;
-  } catch (e) {
-    return e as Error;
-  }
+  });
 };
 
-const query = async (search: string) => {
-  const result = await simpleCompare(search);
-  if (
-    or(and(isBoolean(result), () => NN(result)), () => result instanceof Error)
-  ) {
-    return ["deno.land", result] as const;
-  }
-
-  return ["deno.land", false] as const;
-};
+const query = (search: string): Promise<boolean> | Error =>
+  simpleCompare(search);
 
 export { query, simpleCompare };
