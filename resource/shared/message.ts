72bae1834b1d1e790f1ffa36aad5f0b5d833d4d6
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2021-05-19 15:06:02 +0900

    :recycle:  Move to shared dir

---
+++ resource/shared/message.ts
@@ -0,0 +1,4 @@
+export const INVALID_NOT_STRING = "NAME must be a string";
+export const INVALID_LENGTH_0 = "Name length must be greater than zero";
+export const INVALID_TRIMABLE =
+  "name cannot contain leading or trailing spaces";
