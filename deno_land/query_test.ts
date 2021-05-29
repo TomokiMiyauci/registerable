b36758fcaaa1a7fed0171fa9083d11dbae40c18f
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2021-05-29 15:58:48 +0900

    :pencil2:  Fix typo

--- deno_land/query_test.ts
+++ deno_land/query_test.ts
@@ -1,6 +1,6 @@
 // Copyright 2021-present the Registerable authors. All rights reserved. MIT license.
 import { simpleCompare } from "./query.ts";
-import { assertEquals } from "../../dev_deps.ts";
+import { assertEquals } from "../dev_deps.ts";
 Deno.test("simpleCompare", async () => {
   assertEquals(await simpleCompare("fonction"), false);
   assertEquals(
