f8f4dd3c7c2c3aa5de5d4ce1017ad6c6f3c82b51
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2021-06-02 19:17:00 +0900

    :bento: (web) Remove unused styles

--- website/vite.config.ts
+++ website/vite.config.ts
@@ -5,6 +5,7 @@ import Components from 'vite-plugin-components'
 import ViteIcons, { ViteIconsResolver } from 'vite-plugin-icons'
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/website/',
   plugins: [
     vue(),
     WindiCSS(),
