b6b1c9a27d98ebbec0029769dd1395ee3d169a18
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2021-06-04 22:48:56 +0900

    :children_crossing: (web) Add smoothscroll polyfil

--- website/src/main.ts
+++ website/src/main.ts
@@ -2,5 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import 'virtual:windi.css'
 import './assets/style.css'
+import smoothscroll from 'smoothscroll-polyfill'
 
+// kick off the polyfill!
+smoothscroll.polyfill()
 createApp(App).mount('#app')
