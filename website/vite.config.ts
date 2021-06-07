609c45afbe27b6ad491dcfcb12af09a3f81244f5
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2021-06-07 13:36:33 +0900

    :wrench:  Change vercel config

--- website/vite.config.ts
+++ website/vite.config.ts
@@ -4,7 +4,6 @@ import Components from 'vite-plugin-components'
 import ViteIcons, { ViteIconsResolver } from 'vite-plugin-icons'
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/website/',
   plugins: [
     WindiCSS(),
     Components({
