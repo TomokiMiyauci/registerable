d813e5cba27f53615446979c1a63d73124ff5692
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2021-06-07 18:06:26 +0900

    :lipstick: (web) Change UI

--- website/src/App.vue
+++ website/src/App.vue
@@ -1,185 +1,177 @@
 <template>
-  <div class="container mx-auto lg:px-8">
-    <section class="px-4 text-center pt-10 py-4 md:(pt-30)">
-      <h1 class="text-4xl my-3 font-bold">Registerable</h1>
-      <p id="anchor">Check if package name can be registered</p>
+  <section class="px-4 text-center pt-10 py-4 md:(pt-30)">
+    <h1 class="text-4xl my-3 font-bold">Registerable</h1>
+    <p id="anchor">Check if package name can be registered</p>
 
-      <div class="mt-8 md:my-12 m-1">
-        <span
-          title="Search"
+    <div class="mt-8 md:my-12 m-1">
+      <span
+        title="Search"
+        class="
+          flex
+          items-center
+          mx-auto
+          max-w-2xl
+          overflow-hidden
+          shadow
+          rounded-full
+          transition
+          duration-300
+          transform
+          dark:bg-gray-600
+          hover:(bg-gray-100
+          dark:bg-gray-600
+          dark:opacity-75)
+          focus-within:(ring-4
+          scale-103
+          shadow-md)
+          dark:ring-offset-purple-100
+          ring-gray-400 ring-opacity-70 ring-offset-gray-800 ring-offset-1
+        "
+      >
+        <span class="ml-4">
+          <mdi-magnify class="align-middle w-8 h-8" />
+        </span>
+        <input
+          ontouchstart=""
+          v-model.trim="search"
+          placeholder="Check package name"
+          spellcheck="false"
+          autofocus
+          ref="input"
+          @keydown.enter="onClick"
           class="
+            py-2
+            w-full
+            px-2
+            dark:placeholder-gray-200
+            dark:text-gray-200
+            outline-none
+            text-gray-800
+            bg-transparent
+            ring-fuchsia-300
+          "
+        />
+        <button
+          title="Clear"
+          class="
+            p-2
             flex
-            items-center
-            mx-auto
-            max-w-2xl
-            overflow-hidden
-            shadow
-            rounded-full
             transition
             duration-300
-            transform
-            dark:bg-gray-600
-            hover:(bg-gray-100
-            dark:bg-gray-600
-            dark:opacity-75)
-            focus-within:(ring-4
-            scale-103
+            active:text-blue-600
+            focus:(outline-none
+            bg-blue-200
+            text-blue-500)
+            hover:(bg-blue-200
+            text-blue-400
+            )
+            text-blue-300
+          "
+          @click="onClickClear('')"
+          v-show="searchable"
+        >
+          <mdi-close-circle class="w-7 h-7" />
+        </button>
+        <button
+          class="
+            flex
+            transition
+            duration-300
+            h-full
+            p-2
+            focus:(outline-none
+            bg-gray-200
+            dark:bg-gray-800)
+            not-disabled:hover:(bg-gray-300
+            text-gray-800
+            dark:text-gray-600
+            dark:bg-gray-400)
+            disabled:(text-gray-300
+            dark:text-gray-900
+            cursor-not-allowed)
+          "
+          :disabled="!searchable"
+          @click="onClick"
+        >
+          <mdi-send class="w-7 h-7" />
+        </button>
+      </span>
+    </div>
+
+    <div class="mt-9 md:mt-12 flex justify-center">
+      <template v-for="registry in choiseRegistries">
+        <input
+          class="appearance-none"
+          :id="registry"
+          type="checkbox"
+          v-model="registries"
+          :value="registry"
+        />
+        <label
+          :for="registry"
+          ontouchstart=""
+          :title="registry"
+          class="
+            mx-3
+            cursor-pointer
+            inline-flex
+            opacity-70
+            border
+            dark:border-gray-500
+            p-2
+            w-22
+            h-22
+            bg-gradient-to-br
+            md:(
+            mx-4
+            w-30
+            h-30)
+            hover:(
+            from-purple-100
+            via-pink-100
+            to-yellow-100
+            dark:from-purple-800
+            dark:via-pink-800
+            dark:bg-opacity-30
+            dark:to-yellow-800
             shadow-md)
-            dark:ring-offset-purple-100
-            ring-gray-400 ring-opacity-70 ring-offset-gray-800 ring-offset-1
+            dark:bg-gray-700
+            transform
+            active:scale-110
+            transition
+            duration-300
+            items-center
+            flex-col
+            relative
+            justify-center
+            rounded-md
+            shadow
           "
         >
-          <span class="ml-4">
-            <mdi-magnify class="align-middle w-8 h-8" />
-          </span>
-          <input
-            ontouchstart=""
-            v-model.trim="search"
-            placeholder="Check package name"
-            spellcheck="false"
-            autofocus
-            ref="input"
-            @keydown.enter="onClick"
-            class="
-              py-2
-              w-full
-              px-2
-              dark:placeholder-gray-200
-              dark:text-gray-200
-              outline-none
-              text-gray-800
-              bg-transparent
-              ring-fuchsia-300
-            "
+          <RegistryIcon
+            :icon="registry"
+            class="w-12 h-12 md:(w-16 h-16) text-green-400"
           />
-          <button
-            title="Clear"
-            class="
-              p-2
-              flex
-              transition
-              duration-300
-              active:text-blue-600
-              focus:(outline-none
-              bg-blue-200
-              text-blue-500)
-              hover:(bg-blue-200
-              text-blue-400
-              )
-              text-blue-300
-            "
-            @click="onClickClear('')"
-            v-show="searchable"
+
+          <span
+            class="text-xs md:text-lg mt-1 text-gray-800 dark:text-gray-200"
+            >{{ registry }}</span
           >
-            <mdi-close-circle class="w-7 h-7" />
-          </button>
-          <button
-            class="
-              flex
-              transition
-              duration-300
-              h-full
-              p-2
-              focus:(outline-none
-              bg-gray-200
-              dark:bg-gray-800)
-              not-disabled:hover:(bg-gray-300
-              text-gray-800
-              dark:text-gray-600
-              dark:bg-gray-400)
-              disabled:(text-gray-300
-              dark:text-gray-900
-              cursor-not-allowed)
-            "
-            :disabled="!searchable"
-            @click="onClick"
-          >
-            <mdi-send class="w-7 h-7" />
-          </button>
-        </span>
-      </div>
+        </label>
+      </template>
+    </div>
 
-      <div class="mt-9 md:mt-12 flex justify-center">
-        <template v-for="registry in choiseRegistries">
-          <input
-            class="appearance-none"
-            :id="registry"
-            type="checkbox"
-            v-model="registries"
-            :value="registry"
-          />
-          <label
-            :for="registry"
-            ontouchstart=""
-            :title="registry"
-            class="
-              mx-3
-              cursor-pointer
-              inline-flex
-              opacity-70
-              border
-              dark:border-gray-500
-              p-2
-              w-22
-              h-22
-              bg-gradient-to-br
-              md:(
-              mx-4
-              w-30
-              h-30)
-              hover:(
-              from-purple-100
-              via-pink-100
-              to-yellow-100
-              dark:from-purple-800
-              dark:via-pink-800
-              dark:bg-opacity-30
-              dark:to-yellow-800
-              shadow-md)
-              dark:bg-gray-700
-              transform
-              active:scale-110
-              transition
-              duration-300
-              items-center
-              flex-col
-              relative
-              justify-center
-              rounded-md
-              shadow
-            "
-          >
-            <RegistryIcon
-              :icon="registry"
-              class="w-12 h-12 md:(w-16 h-16) text-green-400"
-            />
+    <h2 class="mt-2 text-lg md:text-2xl max-w-4xl mx-auto">Registry</h2>
+    <p class="text-gray-500 md:mt-2 text-sm md:text-base max-w-4xl mx-auto">
+      Select the registry to query. The smaller the number, the faster.
+    </p>
+  </section>
 
-            <span
-              class="text-xs md:text-lg mt-1 text-gray-800 dark:text-gray-200"
-              >{{ registry }}</span
-            >
-          </label>
-        </template>
-      </div>
-
-      <h2 class="mt-2 text-lg md:text-2xl max-w-4xl mx-auto">Registry</h2>
-      <p class="text-gray-500 md:mt-2 text-sm md:text-base max-w-4xl mx-auto">
-        Select the registry to query. The smaller the number, the faster.
-      </p>
-    </section>
-
-    <transition name="expand">
-      <div ref="div" v-show="!isLoading && resulted" class="p-4">
-        <Result
-          :result="registryPair"
-          :error="state.error"
-          :name="state.name"
-        />
-      </div>
-    </transition>
-  </div>
-
-  <the-footer />
+  <transition name="expand">
+    <div ref="div" v-show="!isLoading && resulted" class="p-4">
+      <Result :result="registryPair" :error="state.error" :name="state.name" />
+    </div>
+  </transition>
 
   <transition name="fade">
     <button
@@ -268,7 +260,6 @@ import { ref, reactive, computed, watch, nextTick, onMounted } from 'vue'
 import { registerable } from 'registerable'
 import type { RegisterableResult } from 'registerable'
 import SearchLoader from './components/SearchLoader.vue'
-import TheFooter from './components/TheFooter.vue'
 import Result from './components/Result.vue'
 import Overlay from './components/Overlay.vue'
 import RegistryIcon from './components/RegistryIcon.vue'
--- website/src/layouts/Default.vue
+++ website/src/layouts/Default.vue
@@ -17,41 +17,63 @@
         dark:border-gray-600
       "
     >
-      <div>
-        <ul>
-          <li>
-            <a
-              class="
-                block
-                py-1.5
-                px-3
-                text-lg
-                rounded-lg
-                text-gray-200
-                bg-gradient-to-br
-                from-purple-400
-                via-pink-500
-                to-yellow-500
-                hover:(
-                opacity-80
-                )
-                transition
-                duration-300
-              "
-              href="/"
-            >
-              Home</a
-            >
-          </li>
-        </ul>
-      </div>
+      <ul>
+        <li>
+          <a
+            class="
+              block
+              py-1.5
+              px-3
+              text-lg
+              rounded-lg
+              text-gray-200
+              bg-gradient-to-br
+              from-purple-400
+              via-pink-500
+              to-yellow-500
+              hover:(
+              opacity-80
+              )
+              transition
+              duration-300
+            "
+            href="/"
+          >
+            Home</a
+          >
+        </li>
+      </ul>
+      <div class="uppercase px-3 mt-6 py-1.5 text-gray-400">api</div>
+      <ul>
+        <li v-for="{ title, to } in links" :key="title">
+          <a :href="to" class="px-3 block py-1.5"> {{ title }} </a>
+        </li>
+      </ul>
     </aside>
-    <Content class="flex-1 h-full" />
+    <div class="flex-1 h-full">
+      <main class="container max-w-5xl mx-auto py-8 px-4 md:px-8 prose">
+        <Content />
+      </main>
+      <TheFooter />
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { useRoute, useRouter } from 'vitepress'
-
 import TheHeader from '../components/TheHeader.vue'
+import TheFooter from '../components/TheFooter.vue'
+const links = [
+  {
+    title: 'CLI',
+    to: '/api/cli/'
+  },
+  {
+    title: 'JavaScript API',
+    to: '/api/javascript/'
+  },
+  {
+    title: 'RESTful API',
+    to: '/api/restful/'
+  }
+]
 </script>
